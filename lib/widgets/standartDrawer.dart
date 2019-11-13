7e8fab53cbce1f201f40881785d7a672b47f0121
hannes <hannes@Hattenbach.de>
2019-11-13 17:15:53 +0100

    Drawer ausgelagert

--- lib/widgets/standartDrawer.dart
+++ lib/widgets/standartDrawer.dart
@@ -12,51 +12,96 @@ class standartDrawer extends StatelessWidget {
   Widget build(BuildContext context) {
     return
       Drawer(
-        child: ListView(
-// Important: Remove any padding from the ListView.
-          padding: EdgeInsets.zero,
+        child: Column(
           children: <Widget>[
-            DrawerHeader(
-              child: Text('On My Way',
-                  style: (TextStyle(
-                      color: Colors.white,
-                      fontSize: 20.2
-                  ))),
-              decoration: BoxDecoration(
-                  color: Colors.teal[200],
-                  gradient: LinearGradient(
-                    colors: [Colors.teal, Colors.teal[100]],
-                    begin: Alignment.topLeft,
-                    end: Alignment.bottomRight,
-                  )
-              ),
-            ),
-            ListTile(
-              title: Row(
+            Expanded(
+              // ListView contains a group of widgets that scroll inside the drawer
+              child: ListView(
+// Important: Remove any padding from the ListView.
+                padding: EdgeInsets.zero,
                 children: <Widget>[
-                  Icon(Icons.settings),
-                  SizedBox(width: 8.0,),
-                  Text('Einstellungen'),
-                ],
-              ),
-              onTap: () {
+                  DrawerHeader(
+                    child: Text('On My Way',
+                        style: (TextStyle(
+                            color: Colors.white,
+                            fontSize: 20.2
+                        ))),
+                    decoration: BoxDecoration(
+                        color: Colors.teal[200],
+                        gradient: LinearGradient(
+                          colors: [Colors.teal, Colors.teal[100]],
+                          begin: Alignment.topLeft,
+                          end: Alignment.bottomRight,
+                        )
+                    ),
+                  ),
+                  ListTile(
+                    title: Row(
+                      children: <Widget>[
+                        Icon(Icons.settings),
+                        SizedBox(width: 8.0,),
+                        Text('Einstellungen'),
+                      ],
+                    ),
+                    onTap: () {
 // Update the state of the app.
 // ...
-              },
-            ),
-            ListTile(
-              title: Row(
-                children: <Widget>[
-                  Icon(Icons.person),
-                  SizedBox(width: 8.0,),
-                  Text('Anmelden'),
-                ],
-              ),
-              onTap: () {
+                    },
+                  ),
+                  ListTile(
+                    title: Row(
+                      children: <Widget>[
+                        Icon(Icons.person),
+                        SizedBox(width: 8.0,),
+                        Text('Anmelden'),
+                      ],
+                    ),
+                    onTap: () {
 // Update the state of the app.
 // ...
-              },
+                    },
+                  ),
+
+                ],
+              ),
             ),
+            // This container holds the align
+            Container(
+              // This align moves the children to the bottom
+                child: Align(
+                    alignment: FractionalOffset.bottomCenter,
+                    // This container holds all the children that will be aligned
+                    // on the bottom and should not scroll with the above ListView
+                    child: Container(
+                        child: Column(
+                          children: <Widget>[
+                            Divider(),
+                            ListTile(
+                              title: Row(
+                                children: <Widget>[
+                                  Icon(Icons.feedback),
+                                  SizedBox(width: 8.0,),
+                                  Text('Feedback'),
+                                ],
+                              ),
+                              onTap: () {
+                              },
+                            ),
+                            ListTile(
+                              title:Row(
+                                children: <Widget>[
+                                  Icon(Icons.supervised_user_circle),
+                                  SizedBox(width: 8.0,),
+                                  Text('Über uns'),
+                                ],
+                              ),
+                              onTap: () {},
+                            ),
+                        ],
+                        )
+                    )
+                )
+            )
           ],
         ),
       )
