86412afff7a75842b691a0b71499fa8ce931ff72
hannes <hannes@Hattenbach.de>
2020-01-23 15:57:48 +0100

    whacky fix 4 locationchooser

--- lib/widgets/Locationchooser.dart
+++ lib/widgets/Locationchooser.dart
@@ -41,15 +41,60 @@ class _LocationchooserS extends State<Locationchooser> {
 
   TextEditingController inputfield = TextEditingController() ;
 
-  Future<http.Response> fetchPost(String searchText,) {
+  Future<Map<String, dynamic>> fetchPost(String searchText,) async {
+    String st=Uri.encodeFull(searchText);
+    var response;
+    try{response =
+    await http.get('https://api.mapbox.com/geocoding/v5/mapbox.places/'+st+'.json?access_token=pk.eyJ1IjoiaGFubmVzb213IiwiYSI6ImNrM3NlbXc4czA0N3Yzbm8xcTF2azdxMzUifQ.qgd9llxTGr6HvXQwcz99Cg'
+        +'&limit=1'
+        +'&language=de'
+        +'&proximity='+longitude.toString()+','+latitude.toString()
+    );}catch(e){
+      onError();
+    }
+
+    if (response.statusCode == 200) {
+      // If server returns an OK response, parse the JSON.
+      return json.decode(response.body);
+    } else {
+      // If that response was not OK, throw an error.
+      throw Exception('Failed to load post');
+    }
+  }
+
+  /*Future<http.Response> fetchPost(String searchText,) {
     String st=Uri.encodeFull(searchText);
     return http.get('https://api.mapbox.com/geocoding/v5/mapbox.places/'+st+'.json?access_token=pk.eyJ1IjoiaGFubmVzb213IiwiYSI6ImNrM3NlbXc4czA0N3Yzbm8xcTF2azdxMzUifQ.qgd9llxTGr6HvXQwcz99Cg'
         +'&limit=1'
         +'&language=de'
         +'&proximity='+longitude.toString()+','+latitude.toString()
     );
+  }*/
+
+  Future<Map<String, dynamic>> fetchLocationName(lat,long) async {
+    String lats=Uri.encodeFull(lat.toString());
+    String longs=Uri.encodeFull(long.toString());
+    var response;
+    try{response =
+    await http.get('https://api.mapbox.com/geocoding/v5/mapbox.places/'+longs+','+lats+'.json?access_token=pk.eyJ1IjoiaGFubmVzb213IiwiYSI6ImNrM3NlbXc4czA0N3Yzbm8xcTF2azdxMzUifQ.qgd9llxTGr6HvXQwcz99Cg'
+        +'&limit=1'
+        +'&language=de'
+        +'&types=address'
+    );}catch(e){
+      onError();
+    }
+
+    if (response.statusCode == 200) {
+      // If server returns an OK response, parse the JSON.
+      return json.decode(response.body);
+    } else {
+      // If that response was not OK, throw an error.
+      onError();
+      throw Exception('Failed to load post');
+    }
   }
 
+  /*
   Future<http.Response> fetchLocationName(lat,long) {
     String lats=Uri.encodeFull(lat.toString());
     String longs=Uri.encodeFull(long.toString());
@@ -58,11 +103,21 @@ class _LocationchooserS extends State<Locationchooser> {
         +'&language=de'
         +'&types=address'
     );
-  }
+  }*/
 
   @override
   void initState() {
     super.initState();
+  }
+
+  bool isok=false;
+
+  void onError(){
+    inputfield.text = "Probier`s nochmal";
+    isok=false;
+  }
+  void onSuccess(coords){
+    isok=true;
   }
 
   @override
@@ -77,18 +132,18 @@ class _LocationchooserS extends State<Locationchooser> {
                 child: TextField(
                   controller: inputfield,
                   onSubmitted: (searchString){
+                    try{
                     fetchPost(searchString).then((value) {
-                      if(value.statusCode==200) { //todo behandlung falls ort nicht gefunden
-                        Map<String, dynamic> body = jsonDecode(value.body);
-                        Map<String, dynamic> feature0 = body['features'][0];
+                        try{Map<String, dynamic> feature0 = value['features'][0];
                         var coords = feature0['center'];
+                        print(coords);
                         var nname = feature0['text_de'];
                         try {
                           mapController.removeCircle(start);
                         } catch (e) {}
                         mapController.addCircle(
                             CircleOptions(
-                              geometry: LatLng(coords[1], coords[0]),
+                              geometry: LatLng(coords[1]+.0, coords[0]+.0),
                               circleColor: "white",
                               circleOpacity: 0.5,
                               circleRadius: 10,
@@ -101,16 +156,18 @@ class _LocationchooserS extends State<Locationchooser> {
                         mapController.animateCamera(
                           CameraUpdate.newCameraPosition(
                             CameraPosition(
-                              target: LatLng(coords[1], coords[0]), zoom: 12.0,
+                              target: LatLng(coords[1]+.0, coords[0]+.0), zoom: 12.0,
                             ),
                           ),
                         );
                         inputfield.text = nname;
-                        print(nname);
-                      }else{
-                        inputfield.text = "Internet Problem";
-                      }
-                    });
+                        onSuccess(coords);
+                        print(nname);}catch(e){onError();}
+                      });
+                    }on Exception catch (err){
+                      onError();
+                    }
+
                   },
                   cursorColor: cGREY,
                   textCapitalization: TextCapitalization.words,
@@ -160,15 +217,15 @@ class _LocationchooserS extends State<Locationchooser> {
                         )
                     ).then((circle){starttmp=circle;});
                     inputfield.text="Adresse wird berechnet..";
-                    fetchLocationName(coordi.latitude,coordi.longitude).then((value) {
-                      if(value.statusCode==200) {//todo behandlung falls ort nicht gefunden
-                        Map<String, dynamic> body = jsonDecode(value.body);
-                        Map<String, dynamic> feature0 = body['features'][0];
+                    try{
+                    fetchLocationName(coordi.latitude,coordi.longitude).then((value) {try{
+                        Map<String, dynamic> feature0 = value['features'][0];
                         var nname = feature0['text_de'];
                         var nnum = feature0['address'];
                         inputfield.text = nname + " " + nnum;
-                        start = starttmp;
-                      }else{
+                        onSuccess(coordi);
+                        start = starttmp;}catch(e){onError();}
+                    });}catch (err){
                         mapController.addCircle(
                             CircleOptions(
                               geometry: coordi,
@@ -179,9 +236,9 @@ class _LocationchooserS extends State<Locationchooser> {
                               circleStrokeWidth: 2,
                             )
                         );
-                        inputfield.text="Internet Error";
+                        onError();
                       }
-                    });
+
                   },
                     gestureRecognizers: <Factory<OneSequenceGestureRecognizer>>[ //todo das verstehen haha copy pasta
                       Factory<OneSequenceGestureRecognizer>(
